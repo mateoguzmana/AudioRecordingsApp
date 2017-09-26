16e5133a077f1e24c4e195238bdb1ef405de9339
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-09-26 12:53:37 -0500

    Install wavesurfer with an example

--- src/components/RecordingsList.jsx
+++ src/components/RecordingsList.jsx
@@ -1,18 +1,26 @@
+require('wavesurfer.js');
+
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Wavesurfer from 'react-wavesurfer';
 
 export default class RecordingsList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            playing: false,
+            pos: 0
         };
+        this.handleTogglePlay = this.handleTogglePlay.bind(this);
+        this.handlePosChange = this.handlePosChange.bind(this);
         this.recordingsList = null;
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getRecordingsList();
     }
 
-    getRecordingsList(){
+    getRecordingsList() {
         //call action for call API and return list
         console.log("return list");
         //this is just an example without get nothing from the server
@@ -27,11 +35,27 @@ export default class RecordingsList extends Component {
         ]
     }
 
+    handleTogglePlay() {
+        this.setState({
+            playing: !this.state.playing
+        });
+    }
+    handlePosChange(e) {
+        this.setState({
+            pos: e.originalArgs[0]
+        });
+    }
 
     render() {
         return (
             <div className="recordingsListPage">
-                Recordings List Page
+                <Wavesurfer
+                    audioFile={'https://wavesurfer-js.org/example/split-channels/stereo.mp3'}
+                    pos={this.state.pos}
+                    onPosChange={this.handlePosChange}
+                    playing={this.state.playing}
+                />
+                <button onClick={this.handleTogglePlay}>Play</button>
             </div>
         );
     }
